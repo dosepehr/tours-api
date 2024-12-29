4d6be0172ff29822ab41028ca40cede0b7c9b89d
dosepehr <dosepehr@gmail.com>
2024-12-29 12:58:31 +0330

    add getting reviws

--- modules/Review/reviewController.js
+++ modules/Review/reviewController.js
@@ -14,9 +14,18 @@ exports.addReview = expressAsyncHandler(async (req, res, next) => {
         data: newReview,
     });
 });
+exports.getReviews = expressAsyncHandler(async (req, res, next) => {
+    const reviews = await Review.find()
+        .populate('tour', '-__v -createdAt -updatedAt')
+        .populate('user', '-__v -createdAt -updatedAt');
+    res.status(200).json({
+        status: true,
+        length: reviews?.length || 0,
+        data: reviews,
+    });
+});
 exports.changeReviewStatus = expressAsyncHandler(async (req, res, next) => {});
 exports.deleteReview = expressAsyncHandler(async (req, res, next) => {});
-exports.getReviews = expressAsyncHandler(async (req, res, next) => {});
 exports.getReview = expressAsyncHandler(async (req, res, next) => {});
 exports.getReviewByTour = expressAsyncHandler(async (req, res, next) => {});
 exports.getReviewByUser = expressAsyncHandler(async (req, res, next) => {});
