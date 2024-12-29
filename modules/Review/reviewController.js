bedb9e2a5d33cfd9d0f040eccf40e6573ce1b0c2
dosepehr <dosepehr@gmail.com>
2024-12-29 12:51:44 +0330

    add adding reviews route

--- modules/Review/reviewController.js
+++ modules/Review/reviewController.js
@@ -1,6 +1,19 @@
 const expressAsyncHandler = require('express-async-handler');
+const Review = require('./reviewModel');
 
-exports.addReview = expressAsyncHandler(async (req, res, next) => {});
+exports.addReview = expressAsyncHandler(async (req, res, next) => {
+    const { review, rating, tour } = req.body;
+    const newReview = await Review.create({
+        review,
+        rating,
+        user: req.user.id,
+        tour,
+    });
+    res.status(201).json({
+        status: true,
+        data: newReview,
+    });
+});
 exports.changeReviewStatus = expressAsyncHandler(async (req, res, next) => {});
 exports.deleteReview = expressAsyncHandler(async (req, res, next) => {});
 exports.getReviews = expressAsyncHandler(async (req, res, next) => {});
